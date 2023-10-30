4fa69db45c77b728a35bed422251c3ce6483e675
omarbengacem <omar.ben-gacem20@imperial.ac.uk>
2023-10-30 11:06:28 +0000

    Added NNclassifier

--- RandomClassifier.py
+++ RandomClassifier.py
@@ -45,9 +45,62 @@ class RandomClassifier:
   def name(self): return self.tree_name
   
   
+
+class KNNClassifier:
+  def __init__(self, dataset):
+    self.unique_labels = []
+    train, test = split_train_test(dataset, test_proportion)
+    self.train = train
+    self.test = test
+    self.tree_name = "Nearest Neighbour Classifier"
+
+  def evaluate_internal(self):
+    
+    out = []
+
+    for test_sample in self.test:
+      solution = test_sample[-1]
+      
+      closest_index = None
+      closest_dist = None
+
+      for train_index, train_sample in enumerate(self.train):
+        
+        dist_sum = 0
+        for feature_index in range( len(train_sample) - 1 ):
+
+          #print("Comparing {a} and {b}".format(a=test_sample[feature_index], b=train_sample[feature_index]))
+          dist_sum += ( test_sample[feature_index] - train_sample[feature_index] ) ** 2
+
+        dist = ( dist_sum ) ** 0.5
+        
+
+        if closest_index is None:
+          closest_index = train_index
+          closest_dist = dist
+        else:
+          #print("Comparing current Distance {a} and new sample {b}".format(a=closest_dist, b=dist))
+          if dist < closest_dist:
+            closest_index = train_index
+            closest_dist = dist
+
+
+
+      #print("Found closest match to be {a} at index {b}".format(a=self.train[closest_index], b=closest_index))
+      if self.train[closest_index][-1] == solution:
+        out.append(1)
+      else:
+        out.append(0)
+
+    return out
+
+
+  def name(self): return self.tree_name
   
-  
-  
+
+
+
+
 def extract_categories(dataset):
   height, width = np.shape(dataset)
   no_of_attrtibutes = width - 1
--- Testbench.py
+++ Testbench.py
@@ -72,7 +72,122 @@ class Testbench():
         plt.grid(True)
         plt.show()
             
-            
+    def confusion_matricies(self):
+        pass
+
+
+
+def confusion_matrix(actuals, predictions, class_labels=None):
+  """ Compute the confusion matrix.
+
+  Args:
+      actual (np.ndarray): the correct ground truth/gold standard labels
+      prediction (np.ndarray): the predicted labels
+      class_labels (np.ndarray): a list of unique class labels.
+                             Defaults to the union of actual and prediction.
+
+  Returns:
+      np.array : shape (C, C), where C is the number of classes.
+                 Rows are ground truth per class, columns are predictions
+  """
+
+  # if no class_labels are given, we obtain the set of unique class labels from
+  # the union of the ground truth annotation and the prediction
+  if not class_labels:
+      class_labels = np.unique(np.concatenate((actuals, predictions)))
+
+  confusion = np.zeros((len(class_labels), len(class_labels)), dtype=int)
+
+  for i in range(len(predictions)):
+    prediction = predictions[i] - 1
+    actual = actuals[i] - 1
+    confusion[prediction][actual] = confusion[prediction][actual] + 1
+
+  return confusion
+
+
+def accuracy(confusion):
+  """ Compute the accuracy given the confusion matrix
+
+  Args:
+      confusion (np.ndarray): shape (C, C), where C is the number of classes.
+                  Rows are ground truth per class, columns are predictions
+
+  Returns:
+      float : the accuracy
+  """
+
+  if np.sum(confusion) > 0:
+      return np.trace(confusion) / np.sum(confusion) # trace <= sum of elements across diagonal
+  else:
+      return 0.
+
+
+def precision(confusion):
+  """ Compute the precision score per class given the ground truth and predictions
+  Also return the macro-averaged precision across classes.
+
+  Returns:
+      tuple: returns a tuple (precisions, macro_precision) where
+          - precisions is a np.ndarray of shape (C,), where each element is the
+            precision for class c
+          - macro-precision is macro-averaged precision (a float)
+  """
+  
+  # Compute the precision per class
+  p = np.diag(confusion) / confusion.sum(axis = 0)
+  # Compute the macro-averaged precision
+  macro_p = np.mean(p)
+
+  return (p, macro_p)
+
+
+def recall(confusion):
+  """ Compute the recall score per class given confusion matrix
+  Also return the macro-averaged recall across classes.
+
+  Returns:
+      tuple: returns a tuple (recalls, macro_recall) where
+          - recalls is a np.ndarray of shape (C,), where each element is the
+              recall for class c
+          - macro-recall is macro-averaged recall (a float)
+  """
+
+  # Compute the recall per class
+  r = np.diag(confusion) / confusion.sum(axis = 1)
+  # Compute the macro-averaged recall
+  macro_r = np.mean(r)
+
+  return (r, macro_r)
+
+
+def f1_score(confusion):
+  """ Compute the F1-score per class given the ground truth and predictions
+
+  Also return the macro-averaged F1-score across classes.
+
+  Args:
+      y_gold (np.ndarray): the correct ground truth/gold standard labels
+      y_prediction (np.ndarray): the predicted labels
+
+  Returns:
+      tuple: returns a tuple (f1s, macro_f1) where
+          - f1s is a np.ndarray of shape (C,), where each element is the
+            f1-score for class c
+          - macro-f1 is macro-averaged f1-score (a float)
+  """
+
+  (precisions, macro_p) = precision(confusion)
+  (recalls, macro_r) = recall(confusion)
+
+  # just to make sure they are of the same length
+  assert len(precisions) == len(recalls)
+  f = 2 * (precisions * recalls) / (precisions + recalls)
+
+  macro_f = np.mean(f)
+
+  return (f, macro_f)
+
     
     
     
