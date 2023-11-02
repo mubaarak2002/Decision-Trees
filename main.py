6b04d630ac60f8b155924544a7a2c1b13607201d
edzchen747 <106090277+edzchen747@users.noreply.github.com>
2023-11-02 12:28:48 +0000

    make dataset inputs more easily configurable

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import Other_Classifiers
 import Testbenches as TB
 import numpy as np
 
-clean_dataset = np.loadtxt("WIFI_db/clean_dataset.txt")
-noisy_dataset = np.loadtxt("WIFI_db/noisy_dataset.txt")
+dataset_path = "WIFI_db/clean_dataset.txt"
+dataset = np.loadtxt(dataset_path)
 
-test = Decision_Tree_Classifier.Tree(clean_dataset)
+test = Decision_Tree_Classifier.Tree(dataset)
 test.show()
 
-#benchmark = TB.Model_Comparison_TB(clean_dataset, 10, Decision_Tree_Classifier.Tree, Other_Classifiers.RandomClassifier, Other_Classifiers.NNClassifier)
+#benchmark = TB.Model_Comparison_TB(dataset, 10, Decision_Tree_Classifier.Tree, Other_Classifiers.RandomClassifier, Other_Classifiers.NNClassifier)
 #benchmark.all_metrics()
 
 
 #benchmark.precision()
 #benchmark.plotNorm()
 
-#test = RandomClassifier.NNClassifier(clean_dataset)
+#test = RandomClassifier.NNClassifier(dataset)
 #print((test.confusion_constructor()[2]))
