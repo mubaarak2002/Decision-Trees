a0bc82052ef93bf3920903ac75bd541ffa5e2ac9
edzchen747 <106090277+edzchen747@users.noreply.github.com>
2023-11-03 02:45:21 +0000

    multiprocessing for hyperparameter tuning

--- main.py
+++ main.py
@@ -5,32 +5,33 @@ import Testbenches as TB
 import numpy as np
 import sys
 
-if len(sys.argv) == 3:
-  dataset_path = sys.argv[1]
-  depth = sys.argv[2]
-else:
-  print("arguements taken: <dataset path> <depth>")
-  quit()
-print("Loading dataset:", dataset_path)
-print("Tree depth:", depth)
-
-dataset = np.loadtxt(dataset_path)
-
-#test = Decision_Tree_Classifier.Tree(dataset, max_depth = depth)
-#print(test.evaluate([[-68,	-58,	-65,	-65,	-76,	-87,	-82,	1]]))
-#test.show()
-
-benchmark = TB.Model_Comparison_TB(dataset, 10, depth, Decision_Tree_Classifier.Tree, Other_Classifiers.RandomClassifier, Other_Classifiers.NNClassifier)
-benchmark.all_metrics()
-
-#benchmark.precision()
-#benchmark.plotNorm()
-
-#test = RandomClassifier.NNClassifier(dataset)
-#print((test.confusion_constructor()[2]))
-
-#hyperParam = TB.Depth_Hyperparameter_Tuning(dataset, Decision_Tree_Classifier.Tree)
-#hyperParam.run()
+def main():
+  if len(sys.argv) == 3:
+    dataset_path = sys.argv[1]
+    depth = sys.argv[2]
+  else:
+    print("arguements taken: <dataset path> <depth>")
+    quit()
+  print("Loading dataset:", dataset_path)
+  print("Tree depth:", depth)
+  
+  dataset = np.loadtxt(dataset_path)
+  
+  #test = Decision_Tree_Classifier.Tree(dataset, max_depth = depth)
+  #print(test.evaluate([[-68,	-58,	-65,	-65,	-76,	-87,	-82,	1]]))
+  #test.show()
+  
+  benchmark = TB.Model_Comparison_TB(dataset, 10, depth, Decision_Tree_Classifier.Tree, Other_Classifiers.RandomClassifier, Other_Classifiers.NNClassifier)
+  benchmark.all_metrics()
+  
+  #benchmark.precision()
+  #benchmark.plotNorm()
+  
+  #test = RandomClassifier.NNClassifier(dataset)
+  #print((test.confusion_constructor()[2]))
+  
+  #hyperParam = TB.Depth_Hyperparameter_Tuning(dataset, Decision_Tree_Classifier.Tree)
+  #hyperParam.run()
 
 if __name__ == '__main__':
     main()
